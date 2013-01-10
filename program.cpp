2a38b3f16c93e291430c1b960feef59f04a568c7
Thalia <thaliacampolina@hotmail.com>
2013-01-10 20:50:22 -0200

    updating program file

--- program.cpp
+++ program.cpp
@@ -1,2 +1,18 @@
+#include <iostream>
+using namespace std;
 
-ioi
+
+
+int main() {
+    int num_period;
+    int num_demanda;
+//MENU
+    cout << "Digite a quantidade de períodos:  ";	
+    cin >> num_period;
+    cout << "Digite a quantidade da demanda:  ";	
+    cin >> num_demanda;
+    cout << "Digite o custo da produção normal:  ";	
+
+
+
+}
